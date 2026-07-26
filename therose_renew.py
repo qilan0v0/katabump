f62df326e95571de132e1b5a2b49bdab61b1fb65
qilan0v0 <qilan0v0@gmail.com>
2026-07-26 18:06:10 +0800

    fix: use uc_click with fallbacks for Order now button

--- therose_renew.py
+++ therose_renew.py
@@ -168,8 +168,13 @@ def renew_servers(sb):
                 btn = sb.find_element('#order-submit', timeout=8)
                 if btn:
                     log("点击 Order now...")
-                    # 使用同步 XHR 提交表单（确保执行完成后再导航）
-                    sb.execute_script("var x=new XMLHttpRequest();x.open('POST','/panel?routeName=cart_renew_buy&id=" + href.split('id=')[1].split('&')[0] + "',false);x.setRequestHeader('Content-Type','application/x-www-form-urlencoded');x.send(new URLSearchParams(new FormData(document.getElementById('renew-form'))));window.location.href=x.responseURL")
+                    try:
+                        sb.uc_click('#order-submit')
+                    except:
+                        try:
+                            sb.click('#order-submit')
+                        except:
+                            btn.click()
                     sb.sleep(5)
                     # 检查续期结果
                     current_url = sb.get_current_url()
